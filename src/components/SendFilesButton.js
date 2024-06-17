f23ec2b322fbc975367292ff5b8b8e9c27382db1
Mohammed Abusaleh <muhammedabusaleh0@gmail.com>
2024-06-17 16:05:58 +0300

    made a mistake and fixed it

--- src/components/SendFilesButton.js
+++ src/components/SendFilesButton.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 function SendFilesButton({ files, setFiles, setSharedFiles }) {
     function handleSnedingFiles() {
-        setSharedFiles((prevFiles) => {
-            const sharedFiles = [...prevFiles, ...files]
+      setSharedFiles((prevFiles) => {
+          const sharedFiles = [...prevFiles, ...files]
 
-            setFiles((prevFiles) => prevFiles.filter(file => !sharedFiles.includes(file)))
+          setFiles((prevFiles) => prevFiles.filter(file => !sharedFiles.includes(file)))
 
-            return sharedFiles
-        })
-        
+          return sharedFiles
+      })      
     }
 
     return (
