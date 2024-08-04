b1de250d7067dd7936a63ab183dadb1c434aa98d
Mohammed Abusaleh <muhammedabusaleh0@gmail.com>
2024-08-04 12:00:42 +0300

    changed structure and added .env logic

--- backend/main.py
+++ backend/main.py
@@ -14,10 +14,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+
 # just for testing remove later
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
